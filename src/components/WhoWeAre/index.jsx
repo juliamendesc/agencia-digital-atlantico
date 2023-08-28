719d7dd3d336a4054024b18ff1b0bc103e150445
Luiz Pereira <leolima.ef@gmail.com>
2023-08-28 12:22:01 +0100

    add animation who we are

--- src/components/WhoWeAre/index.jsx
+++ src/components/WhoWeAre/index.jsx
@@ -1,12 +1,31 @@
+'use client'
 import React from "react";
 import Styles from './who.module.scss'
+import Image from 'next/image';
+import { Fade, Slide } from 'react-awesome-reveal';
+import 'animate.css';
 
+import LightLogo from '../../assets/LightLogo.png';
+import CopLogo from '../../assets/CopLogo.png';
+import LobLogo from '../../assets/LobLogo.png';
+import OrangeLogo from '../../assets/OrangeLogo.png';
+import logoantlantico from '../../assets/logoatlantico.png'
 
 export function Who() {
     return(
     <div id='quemsomos' className={Styles.container}>
-        <h3>Quem Somos</h3>
+        <Slide>
+        <h3><span>Quem Somos</span></h3>
+        </Slide>
+
+        <Fade delay={1e3} cascade damping={1e-1}>
         <h1>Digital Atlântico</h1>
+        <div className={Styles.logoAtla}>
+        <Image src={logoantlantico} width={300}/>
+        </div>
+       
+
+        
         <p>
         Somos uma agência de marketing especializada
         em <span>publicidade online</span>
@@ -36,7 +55,7 @@ export function Who() {
         retorno sobre o investimento <span>(ROI)</span>
         </p>
         </div>
-        
+        </Fade>
         
         <div className={Styles.line}></div>
         
@@ -45,10 +64,24 @@ export function Who() {
         <h2>mais de 30 empresas já tiveram a nossa ajuda.                                                    
         Algumas empresas que já confiam:   
         </h2>
+
         <div className={Styles.logo}>
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
+            <a href="#">
+                <span>Copportunity</span>
+                <Image src={CopLogo} width={200}/>
+            </a>
+            <a href="#">
+                <span>Lights & bottles</span>
+                <Image src={LightLogo} width={205} height={200}/>
+            </a>
+            <a href="#">
+                <span>Lombok</span>
+                <Image src={LobLogo} width={200}/>
+            </a>
+            <a href="#">
+                <span>Orange Store</span>
+                <Image src={OrangeLogo} width={200}/>
+            </a>
         </div>
         </div>
 
