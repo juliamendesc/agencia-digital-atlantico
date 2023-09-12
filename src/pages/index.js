3c0f58ac191f46f22f8e273e4723c7191e04fa42
Julia Mendes <juliamendesc@gmail.com>
2023-09-12 21:23:02 +0100

    put header int every page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
-import Head from 'next/head';
-import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
 import { Section } from '../components/Section';
 import { CardSocial } from '@/components/CardSocial';
 import { FormContact } from '@/components/Form';
 import { Benefits } from '@/components/Benefits';
 import { Who } from '@/components/WhoWeAre';
-import { Faq }  from '@/components/Faq';
+import { Faq } from '@/components/Faq';
 import { Footer } from '@/components/Footer';
+
 export default function Main() {
   return (
     <div>
-      <Header />
       <Hero />
       <Section />
       <CardSocial />
