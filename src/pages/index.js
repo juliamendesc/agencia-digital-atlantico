8e169ccb042c783000643df116618d3360117986
Julia Mendes <juliamendesc@gmail.com>
2023-09-12 21:58:42 +0100

    finish header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
-import Head from 'next/head';
-import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
 import { Section } from '../components/Section';
 import { CardSocial } from '@/components/CardSocial';
 import { FormContact } from '@/components/Form';
 import { Benefits } from '@/components/Benefits';
 import { Who } from '@/components/WhoWeAre';
+import { Faq } from '@/components/Faq';
+import { Footer } from '@/components/Footer';
+
 export default function Main() {
   return (
     <div>
-      <Header />
       <Hero />
       <Section />
       <CardSocial />
       <Benefits />
       <Who />
-{/*       <FormContact />
- */}    </div>
+      <Faq />
+      <FormContact />
+      <Footer />
+    </div>
   );
 }
