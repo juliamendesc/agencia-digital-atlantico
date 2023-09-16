eea314b28ebc7e986166610593f36273ab4f6fad
Luiz Pereira <leolima.ef@gmail.com>
2023-09-16 17:34:04 +0100

    Final Test

--- src/components/CardSocial/index.jsx
+++ src/components/CardSocial/index.jsx
@@ -1,67 +1,59 @@
-'use client'
+'use client';
 import Styles from './cards.module.scss';
-import Image from 'next/image';
-import Facebook from '../../assets/facebook5.png';
-import Instagram from '../../assets/instagram.png';
-import Youtube from '../../assets/youtube.png';
-import Google from '../../assets/google.png';
-
 export function CardSocial() {
-    return(
+  return (
     <div className={Styles.container}>
-        <section className={Styles.cardsSocial}>
-            <div className={Styles.cardFace}>
-            <div className={Styles.row}>
-        <div className={Styles.card}>
-            <h1>Facebook</h1>
-            <p>Ao anunciar no Facebook,
-                    pode atrair novos clientes,
-                    aumentar o reconhecimento da marca,
-                    realizar vendas, entre outras ações.</p>
-        </div>
-    </div>
+      <section className={Styles.cardsSocial}>
+        <div className={Styles.cardFace}>
+          <div className={Styles.row}>
+            <div className={Styles.card}>
+              <h1>Facebook</h1>
+              <p>
+                Ao anunciar no Facebook, pode atrair novos clientes, aumentar o
+                reconhecimento da marca, realizar vendas, entre outras ações.
+              </p>
             </div>
-        
+          </div>
+        </div>
+
         <div className={Styles.cardInsta}>
-        <div className={Styles.row}>
-        <div className={Styles.card}>
-            <h1>Instagram</h1>
-            <p>Ao anunciar no Instagram,
-                pode atrair novos clientes,
-                aumentar o reconhecimento da marca,
-                realizar vendas, entre outras ações.
-                </p>
+          <div className={Styles.row}>
+            <div className={Styles.card}>
+              <h1>Instagram</h1>
+              <p>
+                Ao anunciar no Instagram, pode atrair novos clientes, aumentar o
+                reconhecimento da marca, realizar vendas, entre outras ações.
+              </p>
+            </div>
+          </div>
         </div>
-         </div>
+
+        <div className={Styles.cardYoutube}>
+          <div className={Styles.row}>
+            <div className={Styles.card}>
+              <h1>Youtube</h1>
+              <p>
+                Os anúncios do YouTube permitem a seleção de pessoas por
+                interesse, localidade, palavra-chave ou por conteúdo.
+              </p>
+            </div>
+          </div>
         </div>
-    
-    <div className={Styles.cardYoutube}>
-    <div className={Styles.row}>
-        <div className={Styles.card}>
-            <h1>Youtube</h1>
-            <p>Os anúncios do YouTube permitem a seleção
-            de pessoas por interesse, localidade, 
-            palavra-chave ou por conteúdo. 
-            </p>
-        </div>
-    </div>
-    </div>
-    
+
         <div className={Styles.cardGoogle}>
-        <div className={Styles.row}>
-        <div className={Styles.card}>
-            <h1>Google</h1>
-            <p>O Google é o maior navegador do mundo,
-                com 98% do tráfego de pesquisas. 
-                Anunciar através do Google é uma ferramenta
-                ou estratégia usada para qualquer negócio, 
-                porém para alguns tipos de negócio é fundamental.
-                </p>
+          <div className={Styles.row}>
+            <div className={Styles.card}>
+              <h1>Google</h1>
+              <p>
+                O Google é o maior navegador do mundo, com 98% do tráfego de
+                pesquisas. Anunciar através do Google é uma ferramenta ou
+                estratégia usada para qualquer negócio, porém para alguns tipos
+                de negócio é fundamental.
+              </p>
+            </div>
+          </div>
         </div>
+      </section>
     </div>
-        </div>
-    
-    </section>
-    </div>
-); 
+  );
 }
--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,9 +1,7 @@
 'use client';
 import React from 'react';
 import styles from './section.module.scss';
-import Image from 'next/image';
 import Button from '../Button';
-// import { Fade, Slide } from 'react-awesome-reveal';
 import 'animate.css';
 
 export function Section() {
@@ -12,75 +10,63 @@ export function Section() {
       <main className={styles.main}>
         <section className={styles.parallax1}>
           <div className={styles.textParallax}>
-            {/* <Slide> */}
-              <h3>Serviço</h3>
-              <h1>Como funciona o serviço</h1>
-            {/* </Slide> */}
+            <h3>Serviço</h3>
+            <h1>Como funciona o serviço</h1>
 
-            {/* <Fade delay={1e2} cascade damping={1e-1}> */}
-              <p>
-                Diariamente coletamos dados indicadores, que nos permitem
-                entender os nossos próximos passos, sendo assim possível uma otimização contínua de resultados.
-                </p>
+            <p>
+              Diariamente coletamos dados indicadores, que nos permitem entender
+              os nossos próximos passos, sendo assim possível uma otimização
+              contínua de resultados.
+            </p>
 
-              <p>
-                Realização de análises e pesquisas para compreendermos o
-                público-alvo, as suas necessidades, comportamentos e
-                preferências.
-              </p>
+            <p>
+              Realização de análises e pesquisas para compreendermos o
+              público-alvo, as suas necessidades, comportamentos e preferências.
+            </p>
 
-              <p>
-                A criação e gestão de campanhas de publicidade online. Isso
-                envolve o planeamento de orçamentos, 
-                definição de público-alvo, seleção de palavras-chave, criação de
-                anúncios e otimização contínua das campanhas.
-              </p>
+            <p>
+              A criação e gestão de campanhas de publicidade online. Isso
+              envolve o planeamento de orçamentos, definição de público-alvo,
+              seleção de palavras-chave, criação de anúncios e otimização
+              contínua das campanhas.
+            </p>
 
-              <p>
-                Análise e criação de relatórios, onde utilizamos ferramentas de
-                análise de dados, para monitorar e analisar o desempenho do
-                tráfego e de todas as campanhas ativas, incluindo relatórios com
-                todas as informações detalhadas das métricas.
-              </p>
+            <p>
+              Análise e criação de relatórios, onde utilizamos ferramentas de
+              análise de dados, para monitorar e analisar o desempenho do
+              tráfego e de todas as campanhas ativas, incluindo relatórios com
+              todas as informações detalhadas das métricas.
+            </p>
 
-              <p>
-              Acompanhamento das tendências do mercado, as melhores práticas
-               e as atualizações algorítmicas das 
-              plataformas de anúncios.
-
-              </p>
-            {/* </Fade> */}
+            <p>
+              Acompanhamento das tendências do mercado, as melhores práticas e
+              as atualizações algorítmicas das plataformas de anúncios.
+            </p>
           </div>
-          <Button
-            id={styles.btnMsg}
-            href="/"
-            title="Enviar mensagem"
-          ></Button>
+          <Button id={styles.btnMsg} href="/" title="Enviar mensagem"></Button>
         </section>
 
         <section className={styles.parallax2}>
-          {/* <Slide delay={1e1} cascade> */}
           <div className={styles.textGoals}>
-              <h2>
-                <span>
-                  Estamos preparados para cumprir com nosso único objetivo:
-                </span>
-                </h2>
+            <h2>
+              <span>
+                Estamos preparados para cumprir com nosso único objetivo:
+              </span>
+            </h2>
 
-                <h2>
-                Ajudar o seu negócio e faturamento a crescer de forma
-                exponencial através da internet, com o foco em alcançar novas
-                soluções estratégicas e eficazes para o seu negócio.
-                </h2>
-            </div>
-          {/* </Slide> */}
-          {/* <Fade> */}
-           <div className={styles.textSocial}>
-          <h1> Serviços para a sua empresa 
-            <span>obter resultados</span>
+            <h2>
+              Ajudar o seu negócio e faturamento a crescer de forma exponencial
+              através da internet, com o foco em alcançar novas soluções
+              estratégicas e eficazes para o seu negócio.
+            </h2>
+          </div>
+          <div className={styles.textSocial}>
+            <h1>
+              {' '}
+              Serviços para a sua empresa
+              <span>obter resultados</span>
             </h1>
           </div>
-          {/* </Fade> */}
         </section>
       </main>
     </div>
