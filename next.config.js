94e680a60453d10f01f578c9b528581ff4c1d9be
Julia Mendes <juliamendesc@gmail.com>
2023-09-17 23:47:10 +0100

    add husky

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['github.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  distDir: 'build',
 };
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
