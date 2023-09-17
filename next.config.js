0f5a5562669405e094ddf2f35545e9350562f559
Julia Mendes <juliamendesc@gmail.com>
2023-09-17 23:55:36 +0100

    add contact email script

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
   images: {
     domains: ['github.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
