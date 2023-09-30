e9298b9c29ff65e4290814191a1168c866bcf3dc
Julia Mendes <juliamendesc@gmail.com>
2023-09-30 22:32:22 +0100

    fix header logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,34 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link
+          rel="preload"
+          href="/fonts/Caveat"
+          as="font"
+          type="font/tff"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Lato"
+          as="font"
+          type="font/tff"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Open_Sans"
+          as="font"
+          type="font/tff"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Poppins"
+          as="font"
+          type="font/tff"
+          crossOrigin=""
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body>
